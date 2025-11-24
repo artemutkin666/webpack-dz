99a51aadfd5d156f01f44a138a9d293ce5888289
artem utkin <pigbeniszzzz@gmail.com>
2025-11-24 17:40:58 +0300

    1

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,8 @@
-console.log('app worked');
+import Game, { 
+  GameSavingData, 
+  readGameSaving as loadGame, 
+  writeGameSaving as saveGame 
+} from './game.js';
 
-// Пример модульной функции
-export function greet(name) {
-  return `Hello, ${name}!`;
-}
-
-// Пример класса
-export class Calculator {
-  static add(a, b) {
-    return a + b;
-  }
-  
-  static multiply(a, b) {
-    return a * b;
-  }
-}
-
-// Использование модулей
-const result = Calculator.add(5, 3);
-console.log(`5 + 3 = ${result}`);
+const game = new Game();
+game.start();
